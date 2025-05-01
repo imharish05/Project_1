b4b9e73356c73b552b208c2be763dfcc18071e71
Harish_Sangaran <harish05082004@gmail.com>
2025-05-01 17:23:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function menu(){
+    var side = document.getElementById("close")
+    // console.log("hai")
+    side.style.display = "flex"
+}
+
+function add2(){
+    var side = document.getElementById("close")
+    // console.log("hai")
+    side.style.display = "none"
+}
+function add3(){
+    var side = document.getElementById("list1")
+    // console.log("hai")
+    // side.style.display = "block"
+}
